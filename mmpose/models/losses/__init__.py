bfbefe924e55709a895587180f57c57500191dd2
Miroslav Purkrabek <mira.purkrabek@gmail.com>
2024-01-31 15:32:07 +0100

    experiment with normalized MSE, not completed

--- mmpose/models/losses/__init__.py
+++ mmpose/models/losses/__init__.py
@@ -2,7 +2,7 @@
 from .classfication_loss import BCELoss
 from .heatmap_loss import AdaptiveWingLoss
 from .mesh_loss import GANLoss, MeshLoss
-from .mse_loss import JointsMSELoss, JointsOHKMMSELoss
+from .mse_loss import JointsMSELoss, JointsOHKMMSELoss, JointsNormalizedMSELoss
 from .multi_loss_factory import AELoss, HeatmapLoss, MultiLossFactory
 from .regression_loss import (BoneLoss, L1Loss, MPJPELoss, MSELoss,
                               SemiSupervisionLoss, SmoothL1Loss, SoftWingLoss,
@@ -12,5 +12,6 @@ __all__ = [
     'JointsMSELoss', 'JointsOHKMMSELoss', 'HeatmapLoss', 'AELoss',
     'MultiLossFactory', 'MeshLoss', 'GANLoss', 'SmoothL1Loss', 'WingLoss',
     'MPJPELoss', 'MSELoss', 'L1Loss', 'BCELoss', 'BoneLoss',
-    'SemiSupervisionLoss', 'SoftWingLoss', 'AdaptiveWingLoss'
+    'SemiSupervisionLoss', 'SoftWingLoss', 'AdaptiveWingLoss',
+    'JointsNormalizedMSELoss'
 ]
--- mmpose/models/losses/mse_loss.py
+++ mmpose/models/losses/mse_loss.py
@@ -45,6 +45,97 @@ class JointsMSELoss(nn.Module):
 
 
 @LOSSES.register_module()
+class JointsNormalizedMSELoss(nn.Module):
+    """Normalized MSE loss for heatmaps.
+
+    Args:
+        use_target_weight (bool): Option to use weighted MSE loss.
+            Different joint types may have different target weights.
+        loss_weight (float): Weight of the loss. Default: 1.0.
+    """
+
+    def __init__(self, use_target_weight=False, loss_weight=1.):
+        super().__init__()
+        self.shape_criterion = nn.MSELoss()
+        self.pbt_criterion = nn.CrossEntropyLoss()
+        self.use_target_weight = use_target_weight
+        self.loss_weight = loss_weight
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, output, target, target_weight):
+        """Forward function."""
+        batch_size = output.size(0)
+        num_joints = output.size(1)
+
+        print("Output", output.shape, output.min(), output.max())
+        print("Target", target.shape, target.min(), target.max())
+
+        heatmaps_pred = output.reshape(
+            (batch_size, num_joints, -1))#.split(1, 1)
+        heatmaps_gt = target.reshape((batch_size, num_joints, -1))#.split(1, 1)
+
+        print("Pred heatmaps", heatmaps_pred.shape, heatmaps_pred.min(), heatmaps_pred.max())
+        print("GT   heatmaps", heatmaps_gt.shape, heatmaps_gt.min(), heatmaps_gt.max())
+        print("Target weights", target_weight.shape)
+        
+        # Get probability of joint presence
+        probabilities_pred = self.sigmoid(torch.sum(heatmaps_pred, dim=2))
+        probabilities_gt = self.sigmoid(torch.sum(heatmaps_gt, dim=2))
+        
+        print("Pred probabilities", probabilities_pred.shape, probabilities_pred.min(), probabilities_pred.max())
+        print("GT   probabilities", probabilities_gt.shape, probabilities_gt.min(), probabilities_gt.max())
+
+        # Normalize heatmaps between 0 and 1
+        heatmaps_pred -= heatmaps_pred.min()
+        heatmaps_pred /= heatmaps_pred.max()
+        heatmaps_gt -= heatmaps_gt.min()
+        heatmaps_gt /= heatmaps_gt.max()
+
+        print("Pred norm heatmaps", heatmaps_pred.shape, heatmaps_pred.min(), heatmaps_pred.max())
+        print("GT   nrom heatmaps", heatmaps_gt.shape, heatmaps_gt.min(), heatmaps_gt.max())
+
+        # Split values by kpt_idx
+        heatmaps_pred = heatmaps_pred.split(1, 1)
+        heatmaps_gt = heatmaps_gt.split(1, 1)
+        probabilities_gt = probabilities_gt.split(1, 1)
+        probabilities_pred = probabilities_pred.split(1, 1)
+
+        print("Pred heatmaps", len(heatmaps_pred), heatmaps_pred[0].shape)
+        print("GT   heatmaps", len(heatmaps_gt), heatmaps_gt[0].shape)
+        print("Pred probabilities", len(probabilities_pred), probabilities_pred[0].shape)
+        print("GT   probabilities", len(probabilities_gt), probabilities_gt[0].shape)
+
+        # heatmaps_gt_pbt = np.array([torch.sum(heatmaps_gt[i], dim=2) for i in range(num_joints)])
+        # heatmaps_pred_pbt = np.array([torch.sum(heatmaps_pred[i], dim=2) for i in range(num_joints)])
+
+        # print("Pred heatmaps pbt", heatmaps_pred_pbt.shape, heatmaps_pred_pbt.min(), heatmaps_pred_pbt.max())
+        # print("GT   heatmaps pbt", heatmaps_gt_pbt.shape, heatmaps_gt_pbt.min(), heatmaps_gt_pbt.max())
+
+
+        loss = 0.
+
+        # Check if pred is still grad enabled
+        print("Pred heatmaps", heatmaps_pred[0].requires_grad)
+
+        for idx in range(num_joints):
+            heatmap_pred = heatmaps_pred[idx].squeeze(1)
+            heatmap_gt = heatmaps_gt[idx].squeeze(1)
+            print("Pred heatmap", heatmap_pred.requires_grad)
+            if self.use_target_weight:
+                loss += self.shape_criterion(heatmap_pred * target_weight[:, idx],
+                                       heatmap_gt * target_weight[:, idx])
+                loss += self.pbt_criterion(probabilities_pred[idx] * target_weight[:, idx],
+                                           probabilities_gt[idx] * target_weight[:, idx])
+            else:
+                loss += self.shape_criterion(heatmap_pred, heatmap_gt)
+                loss += self.pbt_criterion(probabilities_pred[idx], probabilities_gt[idx])
+            print("loss", loss.requires_grad)
+
+        # raise NotImplementedError
+        return loss / num_joints * self.loss_weight
+
+
+@LOSSES.register_module()
 class CombinedTargetMSELoss(nn.Module):
     """MSE loss for combined target.
         CombinedTarget: The combination of classification target
